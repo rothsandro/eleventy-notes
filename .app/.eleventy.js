8def3e6fb7382a4ea06d2656b880cec921941f70
Sandro Roth <sandroroth@outlook.com>
2022-12-28 11:02:02 +0100

    refactor: eleventy config loader

--- .app/.eleventy.js
+++ .app/.eleventy.js
@@ -1,18 +1,8 @@
 const loader = require("./_config/utils/loader.util");
 
 module.exports = (eleventyConfig) => {
-  loader(__dirname, {
-    "_config/collections": (col, name) =>
-      eleventyConfig.addCollection(name, col),
-    "_config/libraries": (lib, name) =>
-      eleventyConfig.setLibrary(name, lib(eleventyConfig)),
-    "_config/plugins": (plugin) => eleventyConfig.addPlugin(...plugin),
-    "_config/filters": (filter, name) => eleventyConfig.addFilter(name, filter),
-  });
-
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addWatchTarget("js/**/*.js");
-  eleventyConfig.addWatchTarget("css/**/*.css");
+  // Load eleventy configurations from './_config' folder
+  loader([__dirname, "_config"], eleventyConfig);
 
   return {
     dir: {
--- .app/_config/utils/loader.util.js
+++ .app/_config/utils/loader.util.js
@@ -1,16 +1,33 @@
 const fs = require("fs");
 const path = require("path");
 
+const configs = {
+  collections: function (eleventyConfig, col, name) {
+    eleventyConfig.addCollection(name, col);
+  },
+  libraries: function (eleventyConfig, lib, name) {
+    eleventyConfig.setLibrary(name, lib(eleventyConfig));
+  },
+  plugins: function (eleventyConfig, plugin) {
+    eleventyConfig.addPlugin(...plugin);
+  },
+  filters: function (eleventyConfig, filter, name) {
+    eleventyConfig.addFilter(name, filter);
+  },
+  watchtargets: function (eleventyConfig, watchTarget) {
+    eleventyConfig.addWatchTarget(watchTarget);
+  },
+};
+
 /**
- * Dynamically loads all files in the given folders and calls the handler.
- * It is used to dynamically load collections, filters, shortcodes, etc.
+ * Loads the eleventy configurations from the given directory.
  *
- * @param {string} root The root directory for all provided paths in the config map
- * @param {Record<string, (module: unknown, name: string) => void} configMap
+ * @param {string[]} directory The path to the config folder
+ * @param {object} The eleventy config object
  */
-module.exports = (root, configMap) => {
-  Object.entries(configMap).forEach(([folder, handler]) => {
-    load(path.join(root, folder), handler);
+module.exports = (directory, eleventyConfig) => {
+  Object.entries(configs).forEach(([folder, handler]) => {
+    load(path.join(...directory, folder), handler, eleventyConfig);
   });
 };
 
@@ -18,7 +35,7 @@ function kebabCaseToCamelCase(str) {
   return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
 }
 
-function load(folder, handler) {
+function load(folder, handler, eleventyConfig) {
   const fileNameList = fs.readdirSync(folder);
   fileNameList.forEach((fileName) => {
     const kebabCaseName = fileName.split(".").slice(0, -2).join(".");
@@ -26,6 +43,6 @@ function load(folder, handler) {
     const requirePath = path.join(folder, fileName);
     delete require.cache[require.resolve(requirePath)];
     const module = require(requirePath);
-    handler(module, name);
+    handler(eleventyConfig, module, name);
   });
 }
