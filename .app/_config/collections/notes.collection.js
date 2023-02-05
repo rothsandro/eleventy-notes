478d8e9a44b39836f9d661697fb2a5cfbf1615f7
Sandro Roth <sandroroth@outlook.com>
2023-02-05 11:30:10 +0100

    feat: exclude readme

--- .app/_config/collections/notes.collection.js
+++ .app/_config/collections/notes.collection.js
@@ -1,7 +1,12 @@
+const exclude = new Set(["/readme"]);
+
 module.exports = () => (collectionApi) => {
-  return collectionApi.getFilteredByGlob("../**/*.md").sort((a, b) => {
-    const nameA = a.data.title || a.fileSlug;
-    const nameB = b.data.title || b.fileSlug;
-    return nameA.localeCompare(nameB);
-  });
+  return collectionApi
+    .getFilteredByGlob("../**/*.md")
+    .filter((item) => !exclude.has(item.filePathStem.toLowerCase()))
+    .sort((a, b) => {
+      const nameA = a.data.title || a.fileSlug;
+      const nameB = b.data.title || b.fileSlug;
+      return nameA.localeCompare(nameB);
+    });
 };
