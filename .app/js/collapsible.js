7e86961bed37db0af41b3332e8598ffafd6e8e32
Sandro Roth <sandroroth@outlook.com>
2023-03-04 11:12:40 +0100

    feat: auto-expand tree of active page

--- .app/js/collapsible.js
+++ .app/js/collapsible.js
@@ -4,6 +4,13 @@ Alpine.data("collapsible", (key) => ({
   expanded: Alpine.$persist(true)
     .as(`collapsible:${key}`)
     .using(sessionStorage),
+
+  init() {
+    if (this.$el.querySelector("[aria-current=page]")) {
+      this.expanded = true;
+    }
+  },
+
   async toggle() {
     this.expanded = !this.expanded;
   },
