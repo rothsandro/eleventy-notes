a373ca52cb6574eb61bbc854c6fa74104ea79969
Sandro Roth <sandroroth@outlook.com>
2023-04-02 19:34:17 +0200

    feat: provide normalized tags

--- .app/_config/collections/notes.collection.js
+++ .app/_config/collections/notes.collection.js
@@ -4,9 +4,16 @@ module.exports = (eleventyConfig) => (collectionApi) => {
   const notes = notesCollection(eleventyConfig)(collectionApi);
   return notes.map((note) => ({
     title: note.data.title || note.page.fileSlug,
+    tags: parseTags(note.data.tags),
     fileSlug: note.fileSlug,
     filePathStem: note.filePathStem,
     url: note.url,
     data: note.data,
   }));
 };
+
+function parseTags(tags) {
+  if (Array.isArray(tags)) return tags;
+  if (typeof tags === "string") return [tags];
+  return [];
+}
