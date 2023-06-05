c21a7329b3050c778a983fdbdd0b80eaebe27bc7
Sandro Roth <sandroroth@outlook.com>
2023-06-05 12:32:24 +0200

    feat: improve toggle button styling

--- .app/_config/filters/feather.filter.js
+++ .app/_config/filters/feather.filter.js
@@ -4,5 +4,5 @@ module.exports = () =>
   function (iconName, attr) {
     const icon = icons[iconName];
     if (!icon) throw new Error(`Icon ${iconName} not found`);
-    return icon.toSvg({ width: 18, height: 18, ...attr });
+    return icon.toSvg({ width: "1.125em", height: "1.125em", ...attr });
   };
