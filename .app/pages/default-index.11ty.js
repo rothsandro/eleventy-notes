fca7babffe0d32e3e0163c4f3252b8bd803ef583
Sandro Roth <sandroroth@outlook.com>
2023-08-05 14:30:59 +0200

    refactor: styles into modules

--- .app/pages/default-index.11ty.js
+++ .app/pages/default-index.11ty.js
@@ -13,7 +13,7 @@ class DefaultIndex {
   render() {
     return `
       <main class="app-layout__main" id="app-content">
-        <div class="page content-block">
+        <div class="page">
           <h1 class="page__title">Home</h1>
           Welcome to Eleventy Notes.
           Customize this page by creating a file named index.md in the root of your project.
