f4e84c288dc44a5f6c3379435a9e8d08d4ec09fb
Sandro Roth <sandroroth@outlook.com>
2023-08-23 17:34:33 +0200

    refactor: app config

--- .app/_data/app.js
+++ .app/_data/app.js
@@ -1,66 +1,41 @@
 const fs = require("fs");
 
-const defaultConfig = {
-  title: "Notes",
-  description: "Notes app",
-  theme: {
-    color: "sky",
-  },
-  customProperties: {
-    properties: [],
-  },
-  sidebar: {
-    links: [],
-    notes: [{}],
-  },
-  panel: {
-    tableOfContents: true,
-    customProperties: true,
-    tags: true,
-    incomingLinks: true,
-    outgoingLinks: true,
-    externalLinks: true,
-  },
-  wikilinks: {
-    autoLabel: "ref",
-    anchorLabel: "none",
-  },
-};
-
 module.exports = function () {
   const configPath = "./../app.json";
+  const custom = fs.existsSync(configPath)
+    ? JSON.parse(fs.readFileSync(configPath))
+    : {};
 
-  if (!fs.existsSync(configPath)) return defaultConfig;
-
-  const customConfig = JSON.parse(fs.readFileSync(configPath));
-  const mergedConfig = mergeConfigs(defaultConfig, customConfig);
-
-  return mergedConfig;
-};
-
-function mergeConfigs(a, b) {
   return {
-    ...a,
-    ...b,
-    customProperties: {
-      ...a.customProperties,
-      ...b.customProperties,
-    },
+    title: "Notes",
+    description: "Notes app",
+    ...custom,
     theme: {
-      ...a.theme,
-      ...b.theme,
+      color: "sky",
+      ...custom.theme,
+    },
+    customProperties: {
+      properties: [],
+      ...custom.customProperties,
     },
     sidebar: {
-      ...a.sidebar,
-      ...b.sidebar,
+      links: [],
+      notes: [{}],
+      ...custom.sidebar,
     },
     panel: {
-      ...a.panel,
-      ...b.panel,
+      tableOfContents: true,
+      customProperties: true,
+      tags: true,
+      incomingLinks: true,
+      outgoingLinks: true,
+      externalLinks: true,
+      ...custom.panel,
     },
     wikilinks: {
-      ...a.wikilinks,
-      ...b.wikilinks,
+      autoLabel: "ref",
+      anchorLabel: "none",
+      ...custom.wikilinks,
     },
   };
-}
+};
