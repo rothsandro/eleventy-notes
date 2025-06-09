401174dcdd012d78b297adc83185603db4f9f005
Sandro Roth <16229645+rothsandro@users.noreply.github.com>
2025-06-09 10:55:24 +0200

    refactor: theme generation

--- .app/lib/core/index.js
+++ .app/lib/core/index.js
@@ -2,6 +2,7 @@ import syntaxHighlightPlugin from "@11ty/eleventy-plugin-syntaxhighlight";
 import { markdownLibrary } from "./md.library.js";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 import appData from "./../../_data/app.js";
+import fs from "fs";
 
 export const core = {
   configObj: {
@@ -14,13 +15,37 @@ export const core = {
    * @param {import("@11ty/eleventy").UserConfig} config
    */
   setup(config) {
+    const scssConfig = "./css/config.generated.scss";
+    const customStyles = "./../app.styles.scss";
+
+    let content = fs.existsSync(scssConfig)
+      ? fs.readFileSync(scssConfig, "utf8")
+      : null;
+
+    config.on("eleventy.before", async () => {
+      const appConfig = await import("./../../_data/app.js").then((m) =>
+        m.default()
+      );
+
+      const nextContent = [
+        "// DON'T EDIT THIS FILE!",
+        `$theme: "${appConfig.theme.color}";`,
+        `$custom-styles: ${fs.existsSync(customStyles)};`,
+      ].join("\n");
+
+      if (content === nextContent) return;
+
+      content = nextContent;
+      fs.writeFileSync(scssConfig, content, "utf8");
+    });
+
     config.setLibrary("md", markdownLibrary(config));
 
     config.addPlugin(EleventyHtmlBasePlugin);
     config.addPlugin(syntaxHighlightPlugin);
 
     config.setServerOptions({
-      watch: ["dist/app.js", "dist/app.*.css"],
+      watch: ["dist/app.js", "dist/app.css"],
     });
 
     config.setInputDirectory("./../");
